84bba5516f4513f798a46d5e283a9c06acd7cb85
Kenneth Ferguson <boodaah@me.com>
2020-08-20 00:41:23 -0400

    Fix references to filenames

--- repo2zsh.sh
+++ repo2zsh.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-cp zsh/aliases.txt ~/.zsh_aliases
-cp zsh/prompt.txt ~/.zsh_prompt
+cp zsh/zsh_aliases.txt ~/.zsh_aliases
+cp zsh/zsh_prompt.txt ~/.zsh_prompt
 cp zsh/zshrc.txt ~/.zshrc
